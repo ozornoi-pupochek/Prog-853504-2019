71ac5cda3b1650b971c882d9a570d39262b43c54
Nikita Malashko <47792044+Keckich@users.noreply.github.com>
2019-03-19 13:21:42 +0300

    [Малашко Никита] lab1 variant-22 (#21)
    * Lab01 was added
    
    * Added free function
    
    * First lab was added
    
    * Update lab01.c
    
    * Delete File1.c
    
    * Update lab01.c
    
    * Update lab01.c
    
    * lr1 added
    
    * Lab1 was added
    
    * Delete lab01.c
    
    * Delete lab1.c
    
    * Update File1.c

---
+++ Keckich/lab1/File1.c
@@ -0,0 +1,41 @@
+﻿#include <stdio.h>
+#pragma hdrstop
+#include <tchar.h>
+#include <conio.h>
+#pragma argsused
+int _tmain(int argc, _TCHAR* argv[])
+{
+	unsigned long long numb, i = 1, MainMask, count;
+	printf("Your number:");
+	scanf("%llu",&numb);  // max number to enter: 4 294 967 295
+	MainMask = 0x3;
+
+ while (MainMask < numb) {
+		count = 1;
+   do {
+		if (count % 2) {
+		 printf("11");
+	   }
+		else {
+		 printf("00");
+	   }
+
+		count++;
+
+	  }   while (count <= i) ;
+
+		printf (" - %llu\n", MainMask);
+
+		MainMask = MainMask << 2;
+		i++;
+		if (i % 2 == 0) {
+		  MainMask += 0x0;
+		}
+		else {
+		MainMask += 0x3;
+		}
+	}
+	getch();
+	return 0;
+}
+
